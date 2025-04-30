ed3ab31c0cb66f984e9b0b39e2f80f52f54c3c9e
AmteurPotion <1k0j2i4@gmail.com>
2025-04-30 12:07:13 +0900

    [Feat] Sectioninfo SO 생성
    * 월드내 구역 생성을 위한 스크립터블 오브젝트 생성
    * SectionInfo SO를 받아서 MapManager에서 생성할 수 있도록 구현

--- Assets/Scenes/PersonalWorld/MapManager.cs
+++ Assets/Scenes/PersonalWorld/MapManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using StreamHub.World;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,6 +12,48 @@ namespace StreamHub.Scenes.PersonalWorld
         /// 1 structure
         /// </summary>
         public Tilemap[] layers;
-        
+        public Tilemap Floors => layers[0];
+        public Tilemap Structures => layers[1];
+        [SerializeField, Header("Test Build")]
+        private SectionInfo testSectionInfo;
+        private void Start()
+        {
+            CreateSection(new Vector2Int(0, 0), testSectionInfo);
+        }
+
+        public void CreateSection(Vector2Int sectionPosition, SectionInfo sectionInfo)
+        {
+            List<Vector3Int> floorPositions = new (), structurePositions = new();
+            List<TileBase> floorTiles = new(), structureTiles = new();
+            
+            for (var y = sectionPosition.y; y < sectionInfo.height + sectionPosition.y; y++)
+            {
+                // Set Floor Tile
+                for (var x = sectionPosition.x; x < sectionInfo.width + sectionPosition.x; x++)
+                {
+                    floorPositions.Add(new Vector3Int(x, y, 0));
+                    floorTiles.Add(sectionInfo.floorTile);
+                }
+                
+                // Set Left/Right Border Wall Tile
+                structurePositions.Add(new Vector3Int(sectionPosition.x, y, 0));
+                structureTiles.Add(sectionInfo.wallTile);
+                structurePositions.Add(new Vector3Int(sectionPosition.x + sectionInfo.width, y, 0));
+                structureTiles.Add(sectionInfo.wallTile);
+            }
+            
+            // Set Top/Bottom Border Wall Tile
+            for (var x = sectionPosition.x; x < sectionInfo.width + sectionPosition.x; x++)
+            {
+                structurePositions.Add(new Vector3Int(x, sectionPosition.y, 0));
+                structureTiles.Add(sectionInfo.wallTile);
+                structurePositions.Add(new Vector3Int(x, sectionPosition.y + sectionInfo.height, 0));
+                structureTiles.Add(sectionInfo.wallTile);
+            }
+            
+            // Build Tiles
+            Floors.SetTiles(floorPositions.ToArray(), floorTiles.ToArray());
+            Structures.SetTiles(structurePositions.ToArray(), structureTiles.ToArray());
+        }
     }
 }
---
+++ Assets/Scripts/World/SectionInfo.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace StreamHub.World
+{
+  [CreateAssetMenu(fileName = "new SectionInfo", menuName = "StreamHub/Section Info", order = 0)]
+  public class SectionInfo : ScriptableObject
+  {
+    public TileBase wallTile;
+    public TileBase floorTile;
+    public int width;
+    public int height;
+  }
+}
