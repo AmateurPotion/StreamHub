385547b9a3b1250980e7ff48863a0e58acf1e29d
AmteurPotion <1k0j2i4@gmail.com>
2025-04-30 19:34:02 +0900

    [Feat] 공기 타일 생성
    * 빈 공간을 이동하지 못하게 하는 게 어려워서 걍 공기 타일을 만들었어요

--- Assets/Scenes/PersonalWorld/MapManager.cs
+++ Assets/Scenes/PersonalWorld/MapManager.cs
@@ -44,15 +44,27 @@ namespace StreamHub.Scenes.PersonalWorld
                 structureTiles.Add(sectionInfo.wallTile);
                 structurePositions.Add(new Vector3Int(sectionPosition.x + sectionInfo.width - 1, y));
                 structureTiles.Add(sectionInfo.wallTile);
+                
+                // Set Left/Right Floor layer Hole Tile
+                floorPositions.Add(new Vector3Int(sectionPosition.x - 1, y));
+                floorTiles.Add(sectionInfo.airTile);
+                floorPositions.Add(new Vector3Int(sectionPosition.x + sectionInfo.width, y));
+                floorTiles.Add(sectionInfo.airTile);
             }
             
-            // Set Top/Bottom Border Wall Tile
             for (var x = sectionPosition.x; x < sectionInfo.width + sectionPosition.x; x++)
             {
+                // Set Top/Bottom Border Wall Tile
                 structurePositions.Add(new Vector3Int(x, sectionPosition.y));
                 structureTiles.Add(sectionInfo.wallTile);
                 structurePositions.Add(new Vector3Int(x, sectionPosition.y + sectionInfo.height - 1));
                 structureTiles.Add(sectionInfo.wallTile);
+                
+                // Set Top/Bottom Floor layer Hole Tile
+                floorPositions.Add(new Vector3Int(x, sectionPosition.y - 1));
+                floorTiles.Add(sectionInfo.airTile);
+                floorPositions.Add(new Vector3Int(x, sectionPosition.y + sectionInfo.height));
+                floorTiles.Add(sectionInfo.airTile);
             }
             
             // Build Tiles
