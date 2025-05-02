b92c15f48729f7daf48b32100e5a81930885d22f
AmteurPotion <1k0j2i4@gmail.com>
2025-05-02 17:42:10 +0900

    [Feat] 체력 바 구현
    * 플레이어 최대 / 체력 연동되는 체력 바 구현

--- Assets/Scripts/World/HealthBar.cs
+++ Assets/Scripts/World/HealthBar.cs
@@ -1,9 +1,67 @@
+using System;
+using System.Collections.Generic;
+using StreamHub.Util;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace StreamHub.World
 {
   public class HealthBar : MonoBehaviour
   {
-    
+    [SerializeField] private Sprite full, half, empty;
+    [SerializeField] [GetSet("MaxHp")] private int maxHp;
+    [SerializeField] [GetSet("Hp")] private int hp;
+    [SerializeField] private List<Image> sprites = new();
+
+    public int Hp
+    {
+      get => hp;
+      set
+      {
+        hp = value < 0 ? 0 : value > maxHp ? maxHp : value;
+
+        for (var i = 0; i < sprites.Count; i++)
+          if (i * 2 + 1 == hp) sprites[i].sprite = half;
+          else if (i * 2 < hp) sprites[i].sprite = full;
+          else sprites[i].sprite = empty;
+      }
+    }
+
+    public int MaxHp
+    {
+      get => maxHp;
+      set
+      {
+        for (var i = 0; i < Math.Ceiling((float)Math.Abs(value - maxHp) / 2); i++)
+          if (value > maxHp)
+            CreateHeart();
+          else
+            RemoveHeart();
+
+        maxHp = value < 0 ? 0 : value;
+        Hp = hp;
+      }
+    }
+
+    private void CreateHeart()
+    {
+      var obj = new GameObject("Heart");
+      obj.transform.SetParent(transform);
+      obj.transform.localScale = Vector3.one;
+      obj.transform.localPosition = Vector3.zero;
+      var image = obj.AddComponent<Image>();
+      image.sprite = empty;
+
+      sprites.Add(image);
+    }
+
+    private void RemoveHeart()
+    {
+      if (sprites.Count < 1) return;
+
+      var obj = sprites[^1];
+      sprites.Remove(obj);
+      Destroy(obj.gameObject);
+    }
   }
 }
