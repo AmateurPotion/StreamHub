105eff447a38b064257c3cc0d706d3b8f9127070
AmteurPotion <1k0j2i4@gmail.com>
2025-05-02 11:53:00 +0900

    [Feat] 문 스크립트 추가

---
+++ Assets/Prefabs/Interactable/Door/Door.cs
@@ -0,0 +1,20 @@
+using StreamHub.Scenes.World;
+using UnityEngine;
+
+namespace StreamHub.Prefabs.Interactable.Door
+{
+  public class Door : InteractableObject
+  {
+    [Header("Door")] public bool open;
+    [SerializeField] private Sprite openSprite, closedSprite;
+    [SerializeField] private Collider2D doorCollider;
+
+    public override void Interact(Player player)
+    {
+      base.Interact(player);
+      open = !open;
+      spriteRenderer.sprite = open ? openSprite : closedSprite;
+      doorCollider.enabled = !open;
+    }
+  }
+}
