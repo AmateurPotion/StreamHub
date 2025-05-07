75eb26010d4afc411a35c1e36e9c5e83dc747d38
AmteurPotion <1k0j2i4@gmail.com>
2025-05-07 10:22:01 +0900

    [Feat] 염료 웅덩이 구현

---
+++ Assets/Prefabs/Interactable/NPC/DyePool.cs
@@ -0,0 +1,24 @@
+using StreamHub.Scenes.PersonalWorld;
+using StreamHub.Util;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+namespace StreamHub.Prefabs.Interactable.NPC
+{
+  public class DyePool : InteractableObject
+  {
+    public Color color;
+    public SpriteResolver resolver;
+
+    public void SetSpriteLabel(string label)
+    {
+      resolver.SetCategoryAndLabel(resolver.GetCategory(), label);
+    }
+
+    public override void Interact(Player player)
+    {
+      base.Interact(player);
+      player.color = color;
+    }
+  }
+}
